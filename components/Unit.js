68cafaebcf7195312312564b3843e8a5db3dfbba
Arie <kelivdc@yahoo.com>
2023-06-13 23:59:03 +0700

    Update unit

--- components/Unit.js
+++ components/Unit.js
@@ -1,4 +1,4 @@
-import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { Box, Grid, Tab, Tabs, Typography } from "@mui/material";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
@@ -12,7 +12,15 @@ function Unit(props) {
     setValue(newValue);
   };
   return (
-    <Box sx={{ width: "100%", typography: "body1", padding: "10px" }}>
+    <Box
+      sx={{
+        width: "100%",
+        typography: "body1",
+        padding: "10px",
+        maxWidth: "1024px",
+        margin: "auto",
+      }}
+    >
       <Typography
         variant="h4"
         sx={{
@@ -39,7 +47,7 @@ function Unit(props) {
               color: "#349fab",
             }}
           >
-            Type Studio            
+            Type Studio
           </Typography>
           <Typography
             component="p"
@@ -49,27 +57,35 @@ function Unit(props) {
           >
             2 Bedroom, 1 Bathroom, SGA 23m<sup>2</sup>
           </Typography>
-          <Image
-            src="/studio1.png"
-            width={509}
-            height={651}
-            layout="responsive"
-            alt="Studio "
-          />
-          <Image
-            src="/studio2.png"
-            width={509}
-            height={651}
-            layout="responsive"
-            alt="Studio 2"
-          />
-          <Image
-            src="/studio3.png"
-            width={509}
-            height={651}
-            layout="responsive"
-            alt="Studio 3"
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/studio1.png"
+                width={509}
+                height={651}
+                layout="responsive"
+                alt="Studio "
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/studio2.png"
+                width={509}
+                height={651}
+                layout="responsive"
+                alt="Studio 2"
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/studio3.png"
+                width={509}
+                height={651}
+                layout="responsive"
+                alt="Studio 3"
+              />
+            </Grid>
+          </Grid>
         </TabPanel>
         <TabPanel value="a">
           <Typography
@@ -89,27 +105,35 @@ function Unit(props) {
           >
             2 Bedroom, 1 Bathroom, SGA 39m<sup>2</sup>
           </Typography>
-          <Image
-            src="/2br-a1.png"
-            width={509}
-            height={651}
-            layout="responsive"
-            alt="2BR A "
-          />
-          <Image
-            src="/2br-a2.png"
-            width={509}
-            height={651}
-            layout="responsive"
-            alt="2BR A"
-          />
-          <Image
-            src="/2br-a3.png"
-            width={509}
-            height={651}
-            layout="responsive"
-            alt="2BR A"
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/2br-a1.png"
+                width={509}
+                height={651}
+                layout="responsive"
+                alt="2BR A "
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/2br-a2.png"
+                width={509}
+                height={651}
+                layout="responsive"
+                alt="2BR A"
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/2br-a3.png"
+                width={509}
+                height={651}
+                layout="responsive"
+                alt="2BR A"
+              />
+            </Grid>
+          </Grid>
         </TabPanel>
         <TabPanel value="b">
           <Typography
@@ -129,27 +153,35 @@ function Unit(props) {
           >
             2 Bedroom, 1 Bathroom, SGA 39m<sup>2</sup>
           </Typography>
-          <Image
-            src="/2br-b1.png"
-            width={509}
-            height={651}
-            layout="responsive"
-            alt="2BR B"
-          />
-          <Image
-            src="/2br-b2.png"
-            width={509}
-            height={651}
-            layout="responsive"
-            alt="2BR B"
-          />
-          <Image
-            src="/2br-b3.png"
-            width={509}
-            height={651}
-            layout="responsive"
-            alt="2BR B"
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/2br-b1.png"
+                width={509}
+                height={651}
+                layout="responsive"
+                alt="2BR B"
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/2br-b2.png"
+                width={509}
+                height={651}
+                layout="responsive"
+                alt="2BR B"
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/2br-b3.png"
+                width={509}
+                height={651}
+                layout="responsive"
+                alt="2BR B"
+              />
+            </Grid>
+          </Grid>
         </TabPanel>
         <TabPanel value="c">
           <Typography
@@ -169,34 +201,44 @@ function Unit(props) {
           >
             2 Bedroom, 1 Bathroom, SGA 51m<sup>2</sup>
           </Typography>
-          <Image
-            src="/2br-c1.png"
-            width={509}
-            height={651}
-            layout="responsive"
-            alt="2BR C"
-          />
-          <Image
-            src="/2br-c2.png"
-            width={509}
-            height={651}
-            layout="responsive"
-            alt="2BR C"
-          />
-          <Image
-            src="/2br-c3.png"
-            width={509}
-            height={651}
-            layout="responsive"
-            alt="2BR C"
-          />
-          <Image
-            src="/2br-c4.png"
-            width={509}
-            height={651}
-            layout="responsive"
-            alt="2BR C"
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/2br-c1.png"
+                width={509}
+                height={651}
+                layout="responsive"
+                alt="2BR C"
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/2br-c2.png"
+                width={509}
+                height={651}
+                layout="responsive"
+                alt="2BR C"
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/2br-c3.png"
+                width={509}
+                height={651}
+                layout="responsive"
+                alt="2BR C"
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Image
+                src="/2br-c4.png"
+                width={509}
+                height={651}
+                layout="responsive"
+                alt="2BR C"
+              />
+            </Grid>
+          </Grid>
         </TabPanel>
       </TabContext>
     </Box>
