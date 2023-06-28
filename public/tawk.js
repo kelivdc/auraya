fffbd6a3d4205b123b9a4c33e08dcb0b8a0ae9b0
Arie <kelivdc@yahoo.com>
2023-06-28 10:49:24 +0700

    Update Auraya TawkTo

--- public/tawk.js
+++ public/tawk.js
@@ -2,7 +2,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/6493ae21cc26a871b023f7ea/1h3luv4ei';
+s1.src='https://embed.tawk.to/649baabccc26a871b02512fd/1h4015tga';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
